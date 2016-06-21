97c0547c1f27e38a0c689a9acd6c9a1790d2b7d5
lcappelli <lewiscappelli@gmail.com>
2016-06-21 04:37:08 +0000

    Add files via upload

---
+++ FlyShip.js
@@ -0,0 +1,57 @@
+﻿#pragma strict
+var speed : float = 1.0;
+
+public var moveSpeed : float = 10f;
+public var turnSpeed : float = 50f;
+
+function Start () {
+
+}
+
+function Update () {
+
+
+GetComponent.<ConstantForce>().relativeForce.z = Input.GetAxis("Vertical") * speed * Time.deltaTime; // Moves the spaceship forward
+
+
+ if(Input.GetKey(KeyCode.UpArrow))
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    
+    if(Input.GetKey(KeyCode.DownArrow))
+        transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+    
+    if(Input.GetKey(KeyCode.LeftArrow))
+        transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+    
+    if(Input.GetKey(KeyCode.RightArrow))
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+
+
+}
+
+
+/*
+#pragma strict
+
+public var moveSpeed : float = 10f;
+public var turnSpeed : float = 50f;
+
+
+function Update ()
+{
+    if(Input.GetKey(KeyCode.UpArrow))
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    
+    if(Input.GetKey(KeyCode.DownArrow))
+        transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+    
+    if(Input.GetKey(KeyCode.LeftArrow))
+        transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+    
+    if(Input.GetKey(KeyCode.RightArrow))
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+}
+
+
+
+*/
---
+++ Move.js
@@ -0,0 +1,52 @@
+﻿
+	
+
+
+
+var speed:float = 5.0;
+
+var rotateSpeed:float = 5.0;
+
+var bullitPrefab:Transform;
+
+
+function Update () {
+
+
+var controller : CharacterController = GetComponent(CharacterController);
+
+
+
+//Rotate around y axis
+
+transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+
+
+// move forward and backward
+
+var forward = transform.TransformDirection(Vector3.forward);
+
+
+var curSpeed = speed * Input.GetAxis("Vertical");
+
+controller.SimpleMove(forward * curSpeed);
+
+/*
+
+if(Input.GetButtonDown("Jump"))
+	{
+		var bullit = Instantiate(bullitPrefab, GameObject.Find("spawnPoint").transform.position, Quaternion.identity);
+	     
+	     bullit.rigidbody.AddForce(transform.forward * 2000);
+	
+	
+	
+	}
+	*/
+	
+}
+
+
+
+@script RequireComponent(CharacterController)
+
